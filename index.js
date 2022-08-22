79f430b84e5268c4430d79255e1f8d3259f5a2b5
Ryan Bard <john.ryan.bard@gmail.com>
2022-08-22 08:30:20 -0400

    Add impl and tests

--- index.js
+++ index.js
@@ -1,18 +1,164 @@
+function mustHaveEnvVar(name) {
+    const val = process.env[name]
+    if (!val) {
+        throw new Error(`Must specify ${name}!`)
+    }
+    return val
+}
+
 const port = process.env.PORT || '3000'
+const cfgClientId = mustHaveEnvVar('CLIENT_ID')
+const cfgClientSecret = mustHaveEnvVar('CLIENT_SECRET')
+const cfgExpiresIn = parseInt(process.env.EXPIRES_IN || '300', 10)
+const cfgJwtSecret = mustHaveEnvVar('JWT_SECRET')
 
+const jwt = require('jsonwebtoken');
 const express = require('express')
+const {
+    createLogger,
+    format,
+    transports,
+} = require('winston')
+const uuid = require('uuid').v4
 
-function log(fmt, ...params) {
-    console.log(fmt, ...params)
+const log = createLogger({
+    level: process.env.LOG_LEVEL || 'debug',
+    format: format.json(),
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    log.add(new transports.Console({
+        format: format.combine(
+            format.colorize(),
+            format.timestamp(),
+            format.splat(),
+            format.printf(({level, message, timestamp}) =>
+                `${timestamp} ${level}: ${message}`
+            ),
+        )
+    }))
+}
+
+function generateAccessToken() {
+    return signJwt()
+}
+
+function signJwt() {
+    const options = {
+        algorithm: 'HS256',
+        expiresIn: cfgExpiresIn + ' seconds',
+        audience: 'example-auth-server',
+        issuer: 'example-auth-server',
+        subject: cfgClientId,
+    }
+    const customClaims = {
+        nonce: uuid(),
+    }
+    return new Promise((resolve, reject) => {
+        jwt.sign(customClaims, cfgJwtSecret, options, (err, token) => {
+            if (err) {
+                return reject(err)
+            }
+            return resolve(token)
+        })
+    })
+}
+
+function validateJwt(token) {
+    const options = {
+        algorithms: ['HS256'],
+        clockTolerance: 60,
+        clockTimestamp: Math.floor(Date.now() / 1000),
+    }
+    return jwt.verify(token, cfgJwtSecret, options)
 }
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/health', (req, res) => res.json({status: 'UP'}))
 
-const server = app.listen(port, () => log('Listening on port', port))
+app.use('/token', (req, res, next) => {
+    const authorization = req.headers.authorization || '';
+    const b64UserPass = authorization.replace(/^Basic /, '').trim();
+    const [username, password] = Buffer.from(b64UserPass, 'base64').toString('utf8').split(':');
+    res.locals.clientId = username;
+    res.locals.clientSecret = password;
+    return next();
+})
+
+app.post('/token', async (req, res) => {
+    log.debug('token called')
+    const clientId = res.locals.clientId || req.body.client_id
+    const clientSecret = res.locals.clientSecret || req.body.client_secret
+
+    if (!clientId || !clientSecret || clientId !== cfgClientId || clientSecret !== cfgClientSecret) {
+        log.debug('invalid credentials')
+        return res.status(401).json({
+            error: 'invalid_client',
+            error_description: 'Invalid client_id / client_secret',
+        })
+    }
+
+    const headerAuthUsed = res.locals.clientId || res.locals.clientSecret
+    const bodyAuthUsed = req.body.client_id || req.body.client_secret
+    if (headerAuthUsed && bodyAuthUsed) {
+        log.warn('client creds specified in header and body')
+        return res.status(400).json({
+            error: 'invalid_request',
+            error_description: 'Must specify client id + secret in only 1 place'
+        })
+    }
+
+    if (!req.body.grant_type) {
+        log.warn('missing grant type')
+        return res.status(400).json({
+            error: 'invalid_grant',
+            error_description: 'Must specify grant_type',
+        })
+    }
+
+    if (req.body.grant_type !== 'client_credentials') {
+        log.warn('wrong grant type')
+        return res.status(400).json({
+            error: 'unsupported_grant_type',
+            error_description: 'Only client_credentials grant_type is supported',
+        })
+    }
+
+    log.debug('everything was valid, trying to return an access token response')
+    try {
+        return res.header('Cache-Control', 'no-store').json({
+            access_token: await generateAccessToken(),
+            expires_in: cfgExpiresIn,
+            token_type: 'Bearer',
+        })
+    } catch (err) {
+        log.error('something went wrong: %O', {message: err.message, stack: err.stack})
+        return res.sendStatus(500)
+    }
+})
+
+app.use('/api/*', (req, res, next) => {
+    const authorization = req.headers.authorization || '';
+    const authToken = authorization.replace(/^Bearer /, '').trim();
+    if (!authToken) {
+        return res.sendStatus(401)
+    }
+    try {
+        res.locals.decodedJwt = validateJwt(authToken)
+    } catch (err) {
+        log.debug('Invalid jwt: %O', {message: err.message, stack: err.stack})
+        return res.sendStatus(401)
+    }
+    return next();
+})
+
+app.get('/api/foo', (req, res) => res.json({message: 'success!'}))
+
+const server = app.listen(port, () => log.info('Listening on port', port))
 
 module.exports = {
     close: function (...args) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,6 @@
 const {expect} = require('chai')
+const qs = require('querystring')
+const jwt = require('jsonwebtoken')
 
 const axios = require('axios').default
 
@@ -9,14 +11,66 @@ function callHealth() {
     return axios.get(`${baseUrl}/health`)
 }
 
+function callToken({auth = '', clientId, clientSecret, grantType}) {
+    const body = {
+        grant_type: grantType,
+    }
+    const options = {
+        method: 'post',
+        baseURL: baseUrl,
+        url: '/token',
+    }
+    if (['body', 'both'].includes(auth)) {
+        body.client_id = clientId
+        body.client_secret = clientSecret
+    }
+    if (['', 'header', 'both'].includes(auth)) {
+        options.auth = {
+            username: clientId,
+            password: clientSecret,
+        }
+    }
+    options.data = qs.stringify(body)
+    return axios(options)
+}
+
+function signJwt() {
+    const options = {
+        algorithm: 'HS256',
+        expiresIn: '300 seconds',
+        audience: 'example-auth-server',
+        issuer: 'example-auth-server',
+        subject: 'whatever',
+    }
+    const customClaims = {}
+    return new Promise((resolve, reject) => {
+        jwt.sign(customClaims, 'invalid-client-secret', options, (err, token) => {
+            if (err) {
+                return reject(err)
+            }
+            return resolve(token)
+        })
+    })
+}
+
+
 describe('OAuth2 Auth Server', () => {
 
     let server
+    let clientId = 'test-client-id'
+    let clientSecret = 'test-client-secret'
+    let grantType = 'client_credentials'
+    let expiresIn = 60
 
     before(() => {
         if (!server) {
             try {
                 process.env.PORT = port
+                process.env.LOG_LEVEL = 'error'
+                process.env.CLIENT_ID = clientId
+                process.env.CLIENT_SECRET = clientSecret
+                process.env.EXPIRES_IN = '' + expiresIn
+                process.env.JWT_SECRET = 'foobar'
                 delete require.cache[require.resolve('../index')]
                 server = require('../index')
             } catch (e) {
@@ -39,6 +93,156 @@ describe('OAuth2 Auth Server', () => {
             const resp = await callHealth()
             expect(resp.status).to.eql(200)
             expect(resp.data).to.eql({status: 'UP'})
+        })
+    })
+
+    /*
+     * https://www.rfc-editor.org/rfc/rfc6749.html#section-4.4
+     * https://www.rfc-editor.org/rfc/rfc6749.html#section-5.1
+     * https://www.rfc-editor.org/rfc/rfc6749.html#section-5.2
+     */
+    describe('Token', () => {
+        it('should be successful when client id + secret are in the body', async () => {
+            const resp = await callToken({auth: 'body', clientId, clientSecret, grantType})
+            expect(resp.status).to.eql(200)
+            expect(resp.data.token_type).to.eql('Bearer')
+            expect(resp.data.access_token).to.not.be.undefined
+            expect(resp.data.refresh_token).to.be.undefined
+            expect(resp.data.expires_in).to.eql(expiresIn)
+        })
+
+        it('should be successful when client id + secret are in the auth header', async () => {
+            const resp = await callToken({clientId, clientSecret, grantType})
+            expect(resp.status).to.eql(200)
+            expect(resp.data.token_type).to.eql('Bearer')
+            expect(resp.data.access_token).to.not.be.undefined
+            expect(resp.data.refresh_token).to.be.undefined
+            expect(resp.data.expires_in).to.eql(expiresIn)
+        })
+
+        it('should 401 with an error of invalid_client when missing client_id', async () => {
+            try {
+                await callToken({clientId: '', clientSecret, grantType})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(401)
+                expect(resp.data.error).to.eql('invalid_client')
+                expect(resp.data.error_description).contains('client_id')
+            }
+        })
+
+        it('should 401 with an error of invalid_client when missing client_secret', async () => {
+            try {
+                await callToken({clientId, clientSecret: '', grantType})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(401)
+                expect(resp.data.error).to.eql('invalid_client')
+                expect(resp.data.error_description).contains('client_secret')
+            }
+        })
+
+        it('should 400 with an error of invalid_grant when missing grant_type', async () => {
+            try {
+                await callToken({clientId, clientSecret, grantType: ''})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(400)
+                expect(resp.data.error).to.eql('invalid_grant')
+                expect(resp.data.error_description).contains('grant_type')
+            }
+        })
+
+        it('should 400 with an error of unsupported_grant_type when not client_credentials grant_type', async () => {
+            try {
+                await callToken({clientId, clientSecret, grantType: 'password'})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(400)
+                expect(resp.data.error).to.eql('unsupported_grant_type')
+                expect(resp.data.error_description).contains('grant_type')
+            }
+        })
+
+        it('should 400 with an error of invalid_request when client creds are specified in both header and body', async () => {
+            try {
+                await callToken({auth: 'both', clientId, clientSecret, grantType})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(400)
+                expect(resp.data.error).to.eql('invalid_request')
+            }
+        })
+    })
+
+    describe('Foo API', () => {
+        it('should 200 when auth token is valid', async () => {
+            const tokenResp = await callToken({clientId, clientSecret, grantType})
+            const validJwt  = tokenResp.data.access_token
+            const resp = await axios.get(`${baseUrl}/api/foo`, {headers: {Authorization: `Bearer ${validJwt}`}})
+            expect(resp.status).to.eql(200)
+            expect(resp.data.message).to.eql('success!')
+        })
+
+        it('should 401 when no auth is provided', async () => {
+            try {
+                await axios.get(`${baseUrl}/api/foo`)
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(401)
+            }
+        })
+
+        it('should 401 when auth token is malformed', async () => {
+            const invalidJwt = 'todo'
+            try {
+                await axios.get(`${baseUrl}/api/foo`, {headers: {Authorization: `Bearer ${invalidJwt}`}})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(401)
+            }
+        })
+
+        it('should 401 when auth token is well formed, but invalid', async () => {
+            const invalidJwt = await signJwt()
+            try {
+                await axios.get(`${baseUrl}/api/foo`, {headers: {Authorization: `Bearer ${invalidJwt}`}})
+                expect.fail('Unexpected success!')
+            } catch (err) {
+                const resp = err.response
+                if (!resp) {
+                    throw err
+                }
+                expect(resp.status).to.eql(401)
+            }
         })
     })
 })
